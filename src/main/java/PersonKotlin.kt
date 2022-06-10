26e8e8e41f6a242f22032cc2c510035e9ed47c8d
solomanray <96454998+solomanray@users.noreply.github.com>
2022-06-10 12:55:58 +0800

    Kotlin Practicing ---3th commit

---
+++ src/main/java/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.ray
+
+class PersonKotlin {
+    fun greeting(){
+        println("Hello Kotlin")
+    }
+}
+
+//1st level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
