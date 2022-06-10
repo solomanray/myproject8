f0a9819d28e00c2f3a8c59742a55a0af5e21ac90
solomanray <96454998+solomanray@users.noreply.github.com>
2022-06-10 11:41:19 +0800

    Hello with class Person ---2nd commit

--- src/main/java/com/ray/Hello.java
+++ src/main/java/com/ray/Hello.java
@@ -2,6 +2,8 @@ package com.ray;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hi!!!");
+//        System.out.println("Hi!!!");
+        Person p = new Person();
+        p.greeting();
     }
 }
